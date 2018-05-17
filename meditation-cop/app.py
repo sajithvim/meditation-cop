20ecfd96cb3993061d6141b05799d45a422a136e
sajith <sajith.weerakoon@myob.com>
2018-05-17 23:19:41 +1000

    Refactored the code and formatted the text

--- meditation-cop/app.py
+++ meditation-cop/app.py
@@ -8,19 +8,22 @@ app = Chalice(app_name='meditation-cop')
 @app.route('/', methods=['POST', 'GET'], content_types=['application/x-www-form-urlencoded', 'application/json'])
 def invoke_api():
     try:
-        print(app.current_request)
         request = app.current_request
-        print('raw-request', request.raw_body)
         query = decode_query(request.raw_body.decode("utf-8"))
-        print('query', query)
+        if query is None:
+            return process_random_clip()
+        return process_query(query)
     except Exception as e:
         print(e)
-    return invoke()
 
 
-@app.schedule('rate(20 minutes)')
+@app.schedule('rate(2 minutes)')
 def invoke_scheduler(args):
-    return invoke()
+    try:
+        sound_url = get_random_clip_url()
+        post_message_to_slack(sound_url)
+    except Exception as e:
+        print(e)
 
 
 def decode_query(raw_request):
@@ -32,34 +35,94 @@ def decode_query(raw_request):
     return raw_request[starting_index:ending_index]
 
 
-def invoke():
-    print('starting')
-    data = get_sountract_data()
-    rand_number = random.randint(1, len(data))
-    url = find_rand_url(data, rand_number)
-    headers = {'Content-type': 'application/x-www-form-urlencoded'}
-    r = requests.post("https://hooks.slack.com/services/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",
-                      json={"text": "content:" + url}, headers=headers)
-    response = {
+def process_query(query):
+    functions_map = {
+        "help": process_help,
+        "minute": process_minute,
+        "schedule": process_schedule,
+    }
+    query_words = query.split()
+    if query_words is None or len(query_words) < 1:
+        return process_random_clip()
+    runnable_function = functions_map.get(query_words[0])
+    if runnable_function is None:
+        return process_random_clip()
+    return runnable_function(query)
+
+
+def process_help(query):
+    help_text = {
+        "response_type": "in_channel",
+        "text": "*schedule* : helps you to schedule a meditation session \n *minute* : Sets your environment for one minute meditation \n"
+    }
+    return generate_response(help_text)
+
+
+def process_minute(query):
+    item, url = get_random_clip_url()
+    response_content = {
         "response_type": "in_channel",
         "text": url,
         "attachments": [
             {
-                "text": url
+                "title": item + " music for meditation",
+                "image_url": url,
+                "thumb_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPgFSdJB9qPd87KHD_4sIeoyLZiqhdGcuTvA7bjoGMC91EUuAB",
+                "fields": [
+                    {
+                        "title": "Priority",
+                        "value": "High",
+                        "short": False
+                    }
+                ],
+                "text": "Sample material tp aid meditation"
             }
-        ]
+        ],
     }
-    output = Response(body=response,
-                      status_code=200,
-                      headers={'Content-Type': 'application/json'})
-    return output
+    return generate_response(response_content)
+
+
+def process_schedule(query):
+    response_content = {
+        "response_type": "in_channel",
+        "text": "/polly survey ",
+    }
+    return generate_response(response_content)
+
+
+def process_random_clip():
+    item, url = get_random_clip_url()
+    response_content = {
+        "unfurl_media": True,
+        "text": url,
+    }
+    return generate_response(response_content)
+
+
+def generate_response(content):
+    response = Response(body=content,
+                        status_code=200,
+                        headers={'Content-Type': 'application/json'})
+    return response
+
+
+def post_message_to_slack(content):
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    r = requests.post("https://hooks.slack.com/services/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",
+                      json={"text": "content:" + url}, headers=headers)
+
+
+def get_random_clip_url():
+    data = get_sountract_data()
+    rand_number = random.randint(1, len(data))
+    return find_rand_url(data, rand_number)
 
 
 def get_sountract_data():
     sound_map = {
-        "concentration": "https://files.slack.com/files-pri/T02998537-FAR210MGU/download/relaxing_my_mind.mp3",
-        "connecting": "https://files.slack.com/files-pri/T02998537-FAR210MGU/download/relaxing_my_mind.mp3",
-        "relaxation": "https://files.slack.com/files-pri/T02998537-FAR210MGU/download/relaxing_my_mind.mp3",
+        "concentration": "https://soundcloud.com/amr-reda-1/relax-my-mind",
+        "connecting": "https://soundcloud.com/amr-reda-1/relax-my-mind",
+        "relaxation": "https://soundcloud.com/amr-reda-1/relax-my-mind",
     }
     return sound_map
 
@@ -68,6 +131,6 @@ def find_rand_url(data, rand_number):
     count = 0
     for item, url in data.items():
         if count == rand_number:
-            return url
+            return item, url
         count = count + 1
-    return "https://soundcloud.com/amr-reda-1/relax-my-mind"
+    return 'concentration', data['concentration']
